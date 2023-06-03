b8feae0aa76e7982b51012c9eb9e01d1cafb2dc0
124c4a11 <124c4a11@mail.ru>
2023-06-03 12:33:41 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <form>
+      <input />
+
+      <input />
+      <p>This field is required</p>
+
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+export default App;
